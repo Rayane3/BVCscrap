dbd9b8f84d3c67ddedd452318c5023e434bfea38
Rayane3 <75571788+Rayane3@users.noreply.github.com>
2025-05-08 15:29:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
-    README = readme_file.read()
+with open('README.md') as f:
+    long_description = f.read()
 
-
-setup_args = dict(
+setup(
     name='BVCscrap',
     version='0.0.2',
-    description='Python library to scrape financial data from Casablanca Stock Exchange(Bourse des Valeurs de Casablanca)',
-    long_description_content_type="text/markdown",
-    long_description=README,
+    description='Python library to scrape financial data from Casablanca Stock Exchange',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='ANDAM Amine and editor BOUAYAD Rayane',
+    author_email='bouayadr39@gmail.com',
+    url='https://github.com/Rayane3/BVCscrap',
     license='MIT',
     packages=find_packages(include=["BVCscrap", "BVCscrap.*"]),
-    author='ANDAM Amine and edithor BOUAYAD Rayane',
-    author_email='bouayadr39@gmail.com',
-    keywords=["Web scrapping","financial data"],
-    url='https://github.com/AmineAndam04/BVCscrap',
-
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'pandas',
+        'lxml'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
-
-install_requires = ['requests','BeautifulSoup','pandas','json','datetime',"lxml"]
-
